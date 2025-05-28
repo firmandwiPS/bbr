35e7a64d6c8cd5673733a0e6d9a529454e47c5eb
unknown <gghahah55@gmail.com>
2025-05-28 15:20:54 +0700

    rapihin

---
+++ navbar.php
@@ -0,0 +1,64 @@
+<header id="header" class="header d-flex align-items-center fixed-top">
+  <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+    <a href="index.html" class="logo d-flex align-items-center">
+      <i class="bi bi-buildings"></i>
+      <h1 class="sitename">NiceSchool</h1>
+    </a>
+
+    <nav id="navmenu" class="navmenu">
+      <ul>
+        <li><a href="index.php" class="active">Beranda</a></li>
+        <li class="dropdown"><a href="about.html"><span>Tentang</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+          <ul>
+            <li><a href="about.html">Tentang Kami</a></li>
+            <li><a href="admissions.html">Pendaftaran</a></li>
+            <li><a href="academics.html">Akademik</a></li>
+            <li><a href="faculty-staff.html">Dosen &amp; Staf</a></li>
+            <li><a href="campus-facilities.html">Kampus &amp; Fasilitas</a></li>
+          </ul>
+        </li>
+
+        <li><a href="students-life.html">Kehidupan Siswa</a></li>
+        <li><a href="news.html">Berita</a></li>
+        <li><a href="events.html">Acara</a></li>
+        <li><a href="alumni.html">Alumni</a></li>
+        <li class="dropdown"><a href="#"><span>Halaman Lain</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+          <ul>
+            <li><a href="news-details.html">Detail Berita</a></li>
+            <li><a href="event-details.html">Detail Acara</a></li>
+            <li><a href="privacy.html">Kebijakan Privasi</a></li>
+            <li><a href="terms-of-service.html">Syarat Layanan</a></li>
+            <li><a href="404.html">Kesalahan 404</a></li>
+            <li><a href="starter-page.html">Halaman Awal</a></li>
+          </ul>
+        </li>
+
+        <li class="dropdown"><a href="#"><span>Lainnya</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+          <ul>
+            <li><a href="#">Dropdown 1</a></li>
+            <li class="dropdown"><a href="#"><span>Sub Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+              <ul>
+                <li><a href="#">Sub 1</a></li>
+                <li><a href="#">Sub 2</a></li>
+                <li><a href="#">Sub 3</a></li>
+                <li><a href="#">Sub 4</a></li>
+                <li><a href="#">Sub 5</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Dropdown 2</a></li>
+            <li><a href="#">Dropdown 3</a></li>
+            <li><a href="#">Dropdown 4</a></li>
+          </ul>
+        </li>
+
+        <li><a href="contact.html">Kontak</a></li>
+        
+        <!-- Tombol Login -->
+        <li><a href="login.html" class="btn btn-success text-white px-3 py-1 ms-3" style="border-radius: 5px;">Login</a></li>
+      </ul>
+      <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+    </nav>
+
+  </div>
+</header>
